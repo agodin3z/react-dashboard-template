4011cded69e529a2cf241a880f757a2aacdf8dab
agodin3z <pistilloagodinez@gmail.com>
2025-03-04 00:40:27 -0600

    refactor: update main files with mantine providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
+import '@mantine/core/styles.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+
 import App from './App.tsx';
+import { theme } from './theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <ModalsProvider>
+        {/* <I18nextProvider i18n={i18n}> */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        {/* </I18nextProvider> */}
+      </ModalsProvider>
+    </MantineProvider>
   </StrictMode>,
 );
