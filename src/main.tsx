78c5c31736a977ad9e99ccd47343c1e9fa3d27ff
agodin3z <pistilloagodinez@gmail.com>
2025-03-06 01:32:37 -0600

    refactor: configure notifications provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import '@fontsource/inter/index.css';
 import '@fontsource/poppins/index.css';
 import '@fontsource/ibm-plex-mono/index.css';
@@ -8,6 +9,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 
 import App from './App.tsx';
 import { theme } from './theme';
@@ -15,6 +17,7 @@ import { theme } from './theme';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
+      <Notifications limit={3} />
       <ModalsProvider>
         {/* <I18nextProvider i18n={i18n}> */}
         <BrowserRouter>
