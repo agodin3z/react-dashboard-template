ae6ecba3d09cb527669df8bee5e3a4aa23eb8555
agodin3z <pistilloagodinez@gmail.com>
2025-03-07 08:18:06 -0600

    feat(lib): add utilities file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+let navigateFn: (path: string, opts?: { replace?: boolean }) => void;
+
+export const setNavigate = (nav: typeof navigateFn) => {
+  navigateFn = nav;
+};
+
+export const navigate = (to: string, opts?: { replace?: boolean }) => {
+  if (navigateFn) {
+    navigateFn(to, opts);
+  } else {
+    console.warn('Navigate no inicializado');
+  }
+};
