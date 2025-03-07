317b1d6f4930bf94b7536cedb2d2d11fcd4d71fe
agodin3z <pistilloagodinez@gmail.com>
2025-03-07 08:18:58 -0600

    refactor: update main

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,25 @@
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
-import '@fontsource/inter/index.css';
-import '@fontsource/poppins/index.css';
-import '@fontsource/ibm-plex-mono/index.css';
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
+import { NavigationProgress } from '@mantine/nprogress';
 
 import App from './App.tsx';
 import { theme } from './theme';
 
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import '@fontsource/inter/index.css';
+import '@fontsource/poppins/index.css';
+import '@fontsource/ibm-plex-mono/index.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <Notifications limit={3} />
+      <NavigationProgress />
+      <Notifications limit={3} position="top-center" />
       <ModalsProvider>
         {/* <I18nextProvider i18n={i18n}> */}
         <BrowserRouter>
